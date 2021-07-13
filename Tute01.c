3e46e928d206b9f40c30a76ffcb07ecb88aa3004
IT21088582 <87382912+it21088582@users.noreply.github.com>
2021-07-13 16:29:18 +0000

    I have written tutorial 1 exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1,mark2,total,ave;
+
+  printf("Enter marks for your first subject : ");
+  scanf("%d",&mark1);
+
+  printf("Enter marks for your second subject : ");
+  scanf("%d",&mark2);
+
+  total = mark1 + mark2;
+  ave = total/2;
+
+  printf("average marks = %d",ave);
+
   return 0;
 }
 
